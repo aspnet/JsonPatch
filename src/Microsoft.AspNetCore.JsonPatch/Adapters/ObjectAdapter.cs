b4f246a7c23b375e8026c814e20c233efb3ef06b
Thierry Fleury <thierry@codinlab.fr>
2017-02-08 15:06:05 +0100

    Copy makes deep object copy for reference types.

--- src/Microsoft.AspNetCore.JsonPatch/Adapters/ObjectAdapter.cs
+++ src/Microsoft.AspNetCore.JsonPatch/Adapters/ObjectAdapter.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Reflection;
 using Microsoft.AspNetCore.JsonPatch.Exceptions;
 using Microsoft.AspNetCore.JsonPatch.Helpers;
 using Microsoft.AspNetCore.JsonPatch.Internal;
@@ -362,6 +363,22 @@ namespace Microsoft.AspNetCore.JsonPatch.Adapters
             // Get value at 'from' location and add that value to the 'path' location
             if (TryGetValue(operation.from, objectToApplyTo, operation, out propertyValue))
             {
+                if (propertyValue != null && !propertyValue.GetType().GetTypeInfo().IsValueType)
+                {
+                    // Create deep copy
+                    var copyResult = ConversionResultProvider.ConvertTo(propertyValue, propertyValue.GetType());
+                    if (copyResult.CanBeConverted)
+                    {
+                        propertyValue = copyResult.ConvertedInstance;
+                    }
+                    else
+                    {
+                        var error = CreateOperationFailedError(objectToApplyTo, operation.path, operation, Resources.FormatCannotCopyProperty(operation.from));
+                        ErrorReporter(error);
+                        return;
+                    }
+                }
+
                 Add(operation.path,
                     propertyValue,
                     objectToApplyTo,
